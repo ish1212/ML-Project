cff7c0794176344fcd64c7562b93fa2c1d28acd2
krohak <rohaksinghal14@gmail.com>
2018-05-24 15:17:56 +0530

    remove second lstm layer

--- Generate/Gen-script10.py
+++ Generate/Gen-script10.py
@@ -77,10 +77,12 @@ y = to_categorical(next_seq, num_classes= len(int_to_word))
 print('Build model...')
 model = Sequential()
 
-model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
+model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]),
+               #return_sequences=True
+                ))
 model.add(Dropout(0.2))
-model.add(LSTM(256))
-model.add(Dropout(0.2))
+# model.add(LSTM(256))
+# model.add(Dropout(0.2))
 
 model.add(Dense(y.shape[1]))
 model.add(Activation('softmax'))
