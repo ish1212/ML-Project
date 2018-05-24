7b1051cee8c880c312ca669733504ca9f5a6c6cf
krohak <rohaksinghal14@gmail.com>
2018-05-25 01:45:33 +0530

    gen-script10

--- Generate/Gen-script10.py
+++ Generate/Gen-script10.py
@@ -17,12 +17,12 @@ import sys
 
 np.random.seed()
 
-ids = np.load('Generate/quote_matrix10.npy')
+ids = np.load('/home/krohak/Documents/ML/ML-Project/Generate/quote_matrix10.npy')
 #ids = ids[:1000,:]
 ids = ids[1000:1999,:]
 
-int_to_word = np.load('Generate/int_to_word10.npy')
-word_to_int = np.load('Generate/word_to_int10.npy')
+int_to_word = np.load('/home/krohak/Documents/ML/ML-Project/Generate/int_to_word10.npy')
+word_to_int = np.load('/home/krohak/Documents/ML/ML-Project/Generate/word_to_int10.npy')
 
 int_to_word = int_to_word.item()
 word_to_int = word_to_int.item()
@@ -40,7 +40,7 @@ text = np.ndarray.flatten(np.asarray(text))
 
 
 # In[]: cut the text in semi-redundant sequences of maxlen characters
-maxlen = 10
+maxlen = 100
 step = 1
 seq = []
 next_seq = []
@@ -71,8 +71,8 @@ X = np.reshape(seq, (len(seq), maxlen, 1))
 # normalize
 X = X / max_word
 # one hot encode the output variable
-# y = to_categorical(next_seq, num_classes= max_word)
-y = np.reshape(next_seq,(next_seq.shape[0],1)) / max_word
+y = to_categorical(next_seq, num_classes= max_word)
+# y = np.reshape(next_seq,(next_seq.shape[0],1)) / max_word
 
 # In[7]:
 
@@ -81,27 +81,29 @@ print('Build model...')
 model = Sequential()
 
 model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]),
-               return_sequences=True,
+               #return_sequences=True,
                activation='sigmoid'
                ))
 
 
 # model.add(Dropout(0.2))
 # model.add(LSTM(1024))
-model.add(LSTM(y.shape[1]
-               ,activation='sigmoid'
-               ))
+# model.add(LSTM(y.shape[1]
+#                ,activation='sigmoid'
+#                ))
 
 # model.add(Dropout(0.2))
 
 # model.add(Dense(1024))
 
-# model.add(Dense(y.shape[1]))
+model.add(Dense(y.shape[1]))
 model.add(Activation('softmax'))
 
 #optimizer = RMSprop(lr=0.01)
 optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999)
-model.compile(loss='categorical_cross_entropy', optimizer=optimizer)
+model.compile(
+    loss='categorical_crossentropy',
+    optimizer=optimizer)
 
 
 # In[8]:
@@ -114,8 +116,8 @@ def sample(preds, temperature=1.0):
     # exp_preds = np.exp(preds)
     # preds = exp_preds / np.sum(exp_preds)
     # probas = np.random.multinomial(1, preds, 1)
-    # return np.argmax(preds)
-    return preds
+    return np.argmax(preds)
+    # return preds
 
 
 
@@ -145,7 +147,7 @@ def on_epoch_end(epoch, logs):
             preds = model.predict(x_pred, verbose=0)
             preds = preds[0]
             # print(preds.shape)
-            next_index = round(sample(preds, diversity))
+            next_index = sample(preds, diversity)
             print(next_index)
             next_char = int_to_word[next_index]
 
@@ -166,7 +168,7 @@ filepath="Generate/weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 
 model.fit(X, y,
-          batch_size=128,
+          batch_size=256,
           epochs=1000,
           validation_split=0.05,
           callbacks=[print_callback, checkpoint])
