447b2ff104c830a8d57d55fa8809522d6b0c18a1
krohak <krohak@hku.hk>
2018-05-24 20:10:27 +0800

    using temperature again

--- Generate/generate.py
+++ Generate/generate.py
@@ -106,12 +106,12 @@ model.compile(loss='categorical_crossentropy', optimizer=optimizer)
 def sample(preds, temperature=1.0):
     # helper function to sample an index from a probability array
     # preds = preds[1:]
-    # preds = np.asarray(preds).astype('float64')
-    # preds = np.log(preds) / temperature
-    # exp_preds = np.exp(preds)
-    # preds = exp_preds / np.sum(exp_preds)
-    # probas = np.random.multinomial(1, preds, 1)
-    return np.argmax(preds)
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
 
 
 
